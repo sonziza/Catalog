105b24cddb5ed918fe89c9160a3a9c697129c8ed
Oleg <sonziza@yandex.ru>
2021-04-04 20:07:42 +0300

    Создание контекста БД и файла конфигурации

---
+++ Catalog/Context/CatalogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Context
+{
+    class CatalogDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options)
+        :base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
