5309a4bce0e2f5680899221648a9f6382e04164b
Oleg <sonziza@yandex.ru>
2021-04-04 19:57:14 +0300

    Модели данных

---
+++ Catalog/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
