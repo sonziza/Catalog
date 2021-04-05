2450274e4a9f2224d123dc652bd3d0e7003af904
Oleg <sonziza@yandex.ru>
2021-04-05 09:24:42 +0300

    Клиентская часть: подключение к БД и вывод тестовых данных

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Catalog.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Catalog
 {
@@ -6,7 +11,42 @@ namespace Catalog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<CatalogDbContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+
+            using (CatalogDbContext db = new CatalogDbContext(options))
+            {
+                //Выводим список товаров для каждой категории
+                var categories = db.Categories.Include(c => c.Products).ToList();
+
+                foreach (Category category in categories)
+                {
+                    Console.WriteLine($"Категория: {category.Name}");
+
+                    foreach (Product product in category.Products)
+                    {
+                        Console.WriteLine(product.Name);
+                    }
+
+                    Console.WriteLine("------------------");
+                }
+            }
+
+            Console.Read();
         }
     }
 }
